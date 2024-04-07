677e334ae025ead926fe115650b5526a71d0f253
Puhiza Aliu <aliupuhiza@gmail.com>
2024-04-07 21:41:29 +0200

    Routing & creating Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Create from './components/Create';
+import Navbar from './components/NavBar';
 
 function App() {
+  const navWidth = 220
   return (
     <div className="App">
-      <div>Podular</div>
+      <Navbar 
+        drawerWidth={navWidth}
+        content = {
+          <Routes>
+            <Route path="" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/create" element={<Create/>}/>
+          </Routes>
+        }
+      />
     </div>
   );
 }
