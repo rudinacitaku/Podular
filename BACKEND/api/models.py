731d7d16be884a99495db0b2af973cc1fbb12811
rudinacitaku <rudinacitaku@outlook.com>
2024-04-08 17:59:55 +0200

    Basic Models Setup // Creator, Podcast and PodcastCategory models

--- BACKEND/api/models.py
+++ BACKEND/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Creator Model
+class Creator (models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    address=models.TextField(null=True)
+
+#Category
+    class PodcastCategory (models.Model):
+        title=models.CharField(max_length=200)
+        detail=models.TextField(null=True)
+
+        def __str__ (self):
+            return self.title
+        
+#Podcast 
+    class Podcast (models.Model):
+        title=models.CharField(max_length=200)
+        detail=models.TextField(null=True)
+
+        def __str__ (self):
+            return self.title
--- BACKEND/crud/settings.py
+++ BACKEND/crud/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000/'
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'crud.urls'
