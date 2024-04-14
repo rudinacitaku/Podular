c295f9be83324cdbb9bbc19864178ed401611b83
rudinacitaku <rudinacitaku@outlook.com>
2024-04-14 23:34:45 +0200

    Pagination Set Up and Pagination Customizing (Globally in project)

--- BACKEND/crud/settings.py
+++ BACKEND/crud/settings.py
@@ -138,4 +138,7 @@ REST_FRAMEWORK = {
     #'DEFAULT_AUTHENTICATION_CLASSES': [
     #    'rest_framework_simplejwt.authentication.JWTAuthentication',
     #],
+    #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination',
+    'PAGE_SIZE': 1, 
 }
