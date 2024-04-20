cf7ae6f874c5e1e11ba6310f57abe2810bcc30e4
Puhiza Aliu <aliupuhiza@gmail.com>
2024-04-20 22:30:48 +0200

    Rest API: Add Address functionality in Customer Panel

--- frontend/src/components/Customer/AddAddress.js
+++ frontend/src/components/Customer/AddAddress.js
@@ -1,4 +1,49 @@
+//import Sidebar from './Sidebar';
+import { useState } from "react";
+import axios from 'axios';
+const baseUrl='http://127.0.0.1:8000/api';
+
 function AddAddress(){
+    var customer_id=localStorage.getItem('customer_id');
+    const [ErrorMsg,setErrorMsg]=useState('');
+    const [SuccessMsg,setSuccessMsg]=useState('');
+    const [AddressFormData,setAddressFormData]=useState({
+        'address':'',
+        'customer':customer_id
+    });
+    const inputHandler = (event) =>{
+        setAddressFormData({
+            ...AddressFormData,
+            [event.target.name]:event.target.value
+        });
+    };
+    const submitHandler = () =>{
+        const formData=new FormData();
+        formData.append('address',AddressFormData.address);
+        formData.append('customer',AddressFormData.customer);
+
+        axios.put(baseUrl+'/address/',formData)
+        .then(function(response){
+            if(response.status!=201){
+                setErrorMsg('Data not saved');
+                setSuccessMsg('');
+            }else{
+                setErrorMsg('');
+                setSuccessMsg('Data saved');
+                setAddressFormData({
+                    'address':'',
+
+                });
+            }
+           
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+    };
+    const disableBtn=(AddressFormData.address=='');
+    
+    console.log(AddressFormData);
     return(
         <div className="container mt-4">
             <div className="row">
@@ -9,14 +54,14 @@ function AddAddress(){
                     <div className="card">
                         <h4 className="card-header">Add Address</h4>
                         <div className="card-body">
-                        <form>
+                            {ErrorMsg && <p className="alert alert-danger">{ErrorMsg}</p>}
+                            {SuccessMsg && <p className="alert alert-success">{SuccessMsg}</p>}
                             <div className="mb-3">
-                                <label for="address" className="form-label">Address:</label>
-                                <textarea className="form-control" id="address"></textarea>
+                                <label htmlFor="address" className="form-label">Address:</label>
+                                <textarea className="form-control" name="address" onChange={inputHandler} value={AddressFormData.address} id="address"></textarea>
                             </div>
-                            <button type="submit" className="btn btn-success">Submit</button>
-                        </form>
-                    </div>
+                            <button type="button" disabled={disableBtn} onClick={submitHandler} className="btn btn-success">Submit</button>
+                        </div>
                     </div>
                 </div>
             </div>
