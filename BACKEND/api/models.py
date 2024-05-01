03b5671d28cd885c15c38548ea7d0d1d06d02882
elisamjekuu <120344170+elisamjekuu@users.noreply.github.com>
2024-05-01 15:44:29 +0200

    class ProductRating

--- BACKEND/api/models.py
+++ BACKEND/api/models.py
@@ -51,3 +51,15 @@ class PodcastImage(models.Model):
     
     def __str__(self):
           return self.image.url
+
+
+#Product Rating and Reviews 
+class ProductRating(models.Model):
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE,related_name='rating_customers')
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    rating=models.IntegerField()
+    reviews=models.TextField()
+    add_time=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+          return self.reviews
