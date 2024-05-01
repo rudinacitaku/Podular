d3e1049f97d66b1ba54f840b946a5e68368ad9d7
elisamjekuu <120344170+elisamjekuu@users.noreply.github.com>
2024-05-01 17:04:23 +0200

    Update ChangePassword.js

--- frontend/src/components/Customer/ChangePassword.js
+++ frontend/src/components/Customer/ChangePassword.js
@@ -1,1 +1,26 @@
+import React from 'react';
+
+function ChangePassword() {
+  return (
+    <div className='col-md-9 col-12 mb-2'>
+      <div className='card'>
+        <h4 className='card-header'>Change Password</h4>
+        <div className='card-body'>
           <form>
+            <div className="mb-3">
+              <label htmlFor="pwd" className="form-label">New Password</label>
+              <input type="password" className="form-control" id="pwd" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="cpwd" className="form-label">Confirm Password</label>
+              <input type="password" className="form-control" id="cpwd" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ChangePassword;
