beefea03b2e59b70720dc34be46ca10f5ee22fdf
Puhiza Aliu <aliupuhiza@gmail.com>
2024-05-27 14:49:33 +0200

    small changes for podcasts

--- frontend/src/components/PodcastDetail.js
+++ frontend/src/components/PodcastDetail.js
@@ -52,11 +52,11 @@ function PodcastDetail(){
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span className="visually-hidden">Previous</span>
                     </button>
-                    <button className="carousel-control-prev" type="button"
-                    data-bs-target="#productThumbnailSlider" data-bs-slide="prev">
+                     <button className="carousel-control-next" type="button"
+                    data-bs-target="#productThumbnailSlider" data-bs-slide="next">
                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
                         <span className="visually-hidden">Next</span>
-                    </button>
+                    </button> 
                 </div>
                 </div>
                 <div className="col-8">
--- frontend/src/components/SinglePodcast.js
+++ frontend/src/components/SinglePodcast.js
@@ -10,8 +10,10 @@ function SinglePodcast(props){
                      {/* src={props.podcast.image} */}
                 </Link>
                 <div className="card-body">
-                    <h5 className="card-title"><Link to={`/podcast/${props.podcast.title}/${props.podcast.id}`}>{props.podcast.title}</Link></h5>
+                                                        {/* `/podcast/${props.podcast.title}/${props.podcast.id}` */}
+                    <h5 className="card-title"><Link to={`#`}>{props.podcast.title}</Link></h5>
                     <h5 className='card-title text-muted'>Price: {props.podcast.price} Euro</h5>
+                    <h5 className='card-title text-muted'><Link to={`/podcast/${props.podcast.id}`}>details</Link></h5>
                 </div>
                 <div className='card-footer'>
                     <button title="Add to Cart" className='btn btn-success btn-sm'><i
